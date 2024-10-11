dac478c9b118f70fdaa34a3824aab705dbf7bb64
Christopher-OOC <olojedechristopher24@gmail.com>
2024-10-11 17:14:04 +0100

    commit

--- src/main/java/com/example/appsdevelopers/MobileeAppWeb/exceptions/AppExceptionHandler.java
+++ src/main/java/com/example/appsdevelopers/MobileeAppWeb/exceptions/AppExceptionHandler.java
@@ -3,7 +3,6 @@ package com.example.appsdevelopers.MobileeAppWeb.exceptions;
 import com.example.appsdevelopers.MobileeAppWeb.response.ErrorMessage;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -17,6 +16,20 @@ public class AppExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Object> handleAnyException(Exception ex, WebRequest request) {
+
+        String errorMessage = ex.getLocalizedMessage();
+
+        if (errorMessage == null) {
+            errorMessage = ex.toString();
+        }
+
+        ErrorMessage error = new ErrorMessage(new Date(), errorMessage);
+
+        return new ResponseEntity<>(error, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(NullPointerException.class)
+    public ResponseEntity<Object> handleNullPointerException(NullPointerException ex, WebRequest request) {
 
         String errorMessage = ex.getLocalizedMessage();
 
