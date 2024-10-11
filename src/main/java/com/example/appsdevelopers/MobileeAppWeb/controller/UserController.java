7b45460a183cc73498c94d5ccad1f43dcbc96ba2
Christopher-OOC <olojedechristopher24@gmail.com>
2024-10-11 15:45:20 +0100

    commit

--- src/main/java/com/example/appsdevelopers/MobileeAppWeb/controller/UserController.java
+++ src/main/java/com/example/appsdevelopers/MobileeAppWeb/controller/UserController.java
@@ -1,5 +1,8 @@
 package com.example.appsdevelopers.MobileeAppWeb.controller;
 
+import com.example.appsdevelopers.MobileeAppWeb.response.UserRest;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -7,8 +10,23 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     @GetMapping
-    public String getUser() {
-        return "get user was called";
+    public String getUsers(
+            @RequestParam(value = "page", defaultValue = "1", required = false) int page,
+            @RequestParam(value = "limit", defaultValue = "1", required = false) int limit
+    ) {
+        
+        return "get users was called page = " + page + ", limit = " + limit;
+    }
+
+    @GetMapping(value = "/{userId}", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    public ResponseEntity<UserRest> getUser(@PathVariable("userId") String userId) {
+
+        UserRest returnValue = new UserRest();
+        returnValue.setEmail("test@test.com");
+        returnValue.setFirstName("Christopher");
+        returnValue.setLastName("Olojede");
+
+        return ResponseEntity.ok(returnValue);
     }
 
     @PostMapping
---
+++ src/main/java/com/example/appsdevelopers/MobileeAppWeb/request/UserDetailsRequestModel.java
@@ -0,0 +1,4 @@
+package com.example.appsdevelopers.MobileeAppWeb.request;
+
+public class UserDetailsRequestModel {
+}
