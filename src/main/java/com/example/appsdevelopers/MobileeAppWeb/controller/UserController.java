2e6ba1555614ccb49a5ac946e8961e2d8b7b619f
Christopher-OOC <olojedechristopher24@gmail.com>
2024-10-11 15:52:23 +0100

    commit

--- src/main/java/com/example/appsdevelopers/MobileeAppWeb/controller/UserController.java
+++ src/main/java/com/example/appsdevelopers/MobileeAppWeb/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.example.appsdevelopers.MobileeAppWeb.controller;
 
+import com.example.appsdevelopers.MobileeAppWeb.request.UserDetailsRequestModel;
 import com.example.appsdevelopers.MobileeAppWeb.response.UserRest;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -29,9 +30,18 @@ public class UserController {
         return ResponseEntity.ok(returnValue);
     }
 
-    @PostMapping
-    public String createUser() {
-        return "create user was called";
+    @PostMapping(
+            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}
+    )
+    public ResponseEntity<UserRest> createUser(@RequestBody UserDetailsRequestModel userDetails) {
+
+        UserRest returnValue = new UserRest();
+        returnValue.setEmail(userDetails.getEmail());
+        returnValue.setFirstName(userDetails.getFirstName());
+        returnValue.setLastName(userDetails.getLastName());
+
+        return ResponseEntity.ok(returnValue);
     }
 
     @PutMapping
