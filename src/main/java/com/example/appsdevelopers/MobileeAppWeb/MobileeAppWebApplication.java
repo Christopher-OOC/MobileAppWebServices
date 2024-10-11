31139ab022f35e2fcb84ab91fd580d08577c8417
Christopher-OOC <olojedechristopher24@gmail.com>
2024-10-11 07:33:01 +0100

    commit

---
+++ src/main/java/com/example/appsdevelopers/MobileeAppWeb/MobileeAppWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.appsdevelopers.MobileeAppWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileeAppWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileeAppWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/appsdevelopers/MobileeAppWeb/MobileeAppWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.appsdevelopers.MobileeAppWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileeAppWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
